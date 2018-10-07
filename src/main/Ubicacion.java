5bf455246e13e0f054b6eec16cd1a0adef32e1ba
GONRODRIGUEZ <gonzalo.grodriguez@mercadolibre.com>
2018-10-07 20:46:15 -0300

    Se agrega la colision y el metodo crecer

--- src/main/Ubicacion.java
+++ src/main/Ubicacion.java
@@ -1,34 +1,50 @@
 package main;
 
+import java.util.Objects;
+
 public class Ubicacion {
 
 	private int x;
 	private int y;
-	
+
 	Ubicacion(int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
-	
+
 	public int getX() {
 		return x;
 	}
-	
+
 	public int getY() {
 		return y;
 	}
-	
+
 	public void setX(int x) {
 		this.x = x;
 	}
-	
+
 	public void setY(int y) {
 		this.y = y;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "Ubicacion [x=" + x + ", y=" + y + "]";
 	}
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Ubicacion ubicacion = (Ubicacion) o;
+        return x == ubicacion.x &&
+                y == ubicacion.y;
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(x, y);
+    }
 }
