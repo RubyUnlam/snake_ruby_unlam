5c4ae823a80efc3ceb57844781ffc4c2164e35fe
GONRODRIGUEZ <gonzalo.grodriguez@mercadolibre.com>
2018-10-07 08:47:44 -0300

    Remuevo imports no usados

--- src/main/Campo.java
+++ src/main/Campo.java
@@ -2,13 +2,10 @@ package main;
 
 import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.awt.image.ImageObserver;
-import java.util.List;
 
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
--- src/main/Juego.java
+++ src/main/Juego.java
@@ -1,7 +1,5 @@
 package main;
 
-import java.util.ArrayList;
-
 import javax.swing.JFrame;
 
 public class Juego {
