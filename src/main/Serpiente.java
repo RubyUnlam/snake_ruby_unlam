099090add80eba6adb894a7606dad6d1056c5c2f
GONRODRIGUEZ <gonzalo.grodriguez@mercadolibre.com>
2018-10-07 08:46:29 -0300

    Base de la parte visual y la serpiente, todavía le falta mucho

--- src/main/Serpiente.java
+++ src/main/Serpiente.java
@@ -1,5 +1,64 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Serpiente {
+	
+	private List<Ubicacion> ubicaciones = new ArrayList<>();
+	private int mirandoX;
+	private int mirandoY;
+	private int velocidad = 20;
+	
+	Serpiente(){
+		ubicaciones.add(new Ubicacion(50, 90)); //TODO ELIMINAR UBICACIONES DEFAULT
+		ubicaciones.add(new Ubicacion(50, 70));
+		ubicaciones.add(new Ubicacion(50, 50));
+		ubicaciones.add(new Ubicacion(50, 30));
+		ubicaciones.add(new Ubicacion(50, 10));
+		mirarAbajo();
+	}
+	
+	public void moverse() {
+		for(int i = ubicaciones.size() - 1 ; i >= 0 ; i--) {
+			if (i != 0) {
+				ubicaciones.set(i, ubicaciones.get(i-1));
+			} else {
+				Ubicacion cabeza = ubicaciones.get(i);
+				int x = mirandoX != 0 ? cabeza.getX() + (mirandoX * velocidad) : cabeza.getX();
+				int y = mirandoY != 0 ? cabeza.getY() + (mirandoY * velocidad) : cabeza.getY();
+				
+				ubicaciones.set(0, new Ubicacion(x,y));
+			}	
+		}
+	}
+	
+	public void crecer() {
+		
+	}
+	
+	public void mirarDerecha() { //TODO VALIDAR LOS MOVIMIENTOS
+		mirandoX = 1;
+		mirandoY = 0;
+	}
+	
+	public void mirarIzquierda() {
+		mirandoX = -1;
+		mirandoY = 0;
+	}
+	
+	public void mirarArriba() {
+		mirandoX = 0;
+		mirandoY = -1;
+	}
+	
+	public void mirarAbajo() {
+		mirandoX = 0;
+		mirandoY = 1;
+	}
+	
+	public List<Ubicacion> getUbicaciones() {
+		return ubicaciones;
+	}
 
 }
---
+++ src/main/Ubicacion.java
@@ -0,0 +1,34 @@
+package main;
+
+public class Ubicacion {
+
+	private int x;
+	private int y;
+	
+	Ubicacion(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	public void setX(int x) {
+		this.x = x;
+	}
+	
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	@Override
+	public String toString() {
+		return "Ubicacion [x=" + x + ", y=" + y + "]";
+	}
+
+}
