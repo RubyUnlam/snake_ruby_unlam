070e0d76cb670a692f299b0788af77981b9f85b4
GONRODRIGUEZ <gonzalo.grodriguez@mercadolibre.com>
2018-11-25 18:12:40 -0300

    Borrando cosas innecesarias de Juego

--- src/main/Juego.java
+++ src/main/Juego.java
@@ -1,45 +1,28 @@
 package main;
 
-import servidor.Servidor;
-
-import static utilidades.Constantes.ALTURA_VENTANA;
-import static utilidades.Constantes.ANCHO_VENTANA;
-
 import java.awt.Color;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.swing.JFrame;
-
 public class Juego {
 
-	public static void iniciar(Sala sala) {
-		
+    public static void iniciar(Sala sala) {
 
-		List<Serpiente> serpientes = new ArrayList<>();
-		
-		for (int i = 0; i < sala.getCantidadJugadores(); i++) {
-			serpientes.add(new Serpiente(Color.BLUE));			
-		}
-		
-		List<SerpienteIA> serpientesIA = new ArrayList<>();			
-		for (int i = 0; i < sala.getCantidadIA(); i++) {
-			serpientesIA.add(new SerpienteIA(sala.getDificultadIA(), Color.BLACK));
-		}
-		
-//        Ui ui = new Ui();
-        
+
+        List<Serpiente> serpientes = new ArrayList<>();
+
+        for (int i = 0; i < sala.getCantidadJugadores(); i++) {
+            serpientes.add(new Serpiente(Color.BLUE));
+        }
+
+        List<SerpienteIA> serpientesIA = new ArrayList<>();
+        for (int i = 0; i < sala.getCantidadIA(); i++) {
+            serpientesIA.add(new SerpienteIA(sala.getDificultadIA(), Color.BLACK));
+        }
+
+
         Campo campo = new Campo(serpientes, serpientesIA);
 
-//        campo.agregarObservador(ui);
-//        ventana.setContentPane(ui);
-
-	}
-
-//	private void iniciarServidor() {
-//		Servidor servidor = new Servidor(12000, serpientes);
-//
-//		servidor.start();
-//	}
+    }
 
 }
