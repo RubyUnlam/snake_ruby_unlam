adcd044a70555bdbfe9c7f8272db437ab8511f7c
unknown <sebastian.barros@hotmail.com>
2018-12-01 04:20:40 -0300

    Agrego el comestible ciruela

---
+++ src/juego/comestible/Ciruela.java
@@ -0,0 +1,14 @@
+package juego.comestible;
+
+import java.awt.*;
+
+public class Ciruela extends Comestible {
+
+    private final static Color COLOR = Color.MAGENTA;
+    private final static String POWER_UP = "dividir";
+    private final static int PUNTAJE = 5;
+
+    public Ciruela(){
+        super(COLOR, PUNTAJE, POWER_UP);
+    }
+}
