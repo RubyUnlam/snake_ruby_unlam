7c5963dc36ab87bd08af234087d10d444ce60c9c
GONRODRIGUEZ <gonzalo.grodriguez@mercadolibre.com>
2018-12-01 04:47:23 -0300

    Levanto confuracion de un file

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,15 +1,18 @@
 package main;
 
+import excepciones.ErrorConfiguracionException;
 import servidor.Servidor;
+import utilidades.Configuracion;
 
 import static main.SesionSingleton.getSessionFactory;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ErrorConfiguracionException {
         // lo primero que hago antes del levantar el servidor es crear el session factory
         // para evitar penalizar al primer jugador en loguear con la espera de creacion de este
-        new Servidor(12000).start();
+        Configuracion configuracion = new Configuracion("configuracion");
+        new Servidor(configuracion.getPuerto()).start();
         getSessionFactory();
     }
 }
